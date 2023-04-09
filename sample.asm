0872bc989f08c21e8fb02f23d3abbc6394f45588
satyam <satyamatal1000@gmail.com>
2023-04-09 23:08:31 +0530

    theek karo

--- sample.asm
+++ sample.asm
@@ -1,16 +1,14 @@
-lw $1, 100($2)
-# main:
- #	addi	$t0, $0, 10
- #	add		$t1, $0, $0
- #	addi	$s1, $zero, 320
- # loop:
- #	beq		$t1, $t0, exit
- #	sw		$s0, 0($s1)
- #	addi	$s0, $s0, 100
- #	addi	$t1, $t1, 1
- #	j		loop
 
- # exit:
-
-# addi		$t0, $0, 0 error in parsing this command;
-# lw		$t1, 0($t0)
+ main:
+ 	addi $t0,$0,0
+    addi $t1,$t0,0
+    addi $t2,$t1,0
+    add $t3,$t2,$t1
+    addi $t4,$0,0
+    addi $t5,$0,0
+    addi $6,$0,0
+    addi $7,$0,0
+    addi $t8,$0,0
+    addi $s1,$0,0
+    addi $s2,$0,0
+Exit:
--- test.cpp
+++ test.cpp
@@ -19,6 +19,7 @@ struct stage
 
 int detect_EX_hazard(stage a,  stage b)
 {
+	if (a.id<0 || b.id<0){return 0;}
 	if (a.sig == "RW" && (a.rd == b.rs))
 	{
 		return 1;
@@ -34,7 +35,8 @@ int detect_EX_hazard(stage a,  stage b)
 }
 
 int detect_Mem_hazard(stage a, stage b)
-{
+{  
+	if (a.id<0 || b.id<0){return 0;}
 	if (a.sig == "MR" && (a.rd == b.rs))
 	{
 		return 1;
@@ -91,44 +93,74 @@ void assign(stage &a, stage &b){
 	a.rd=b.rd;
 	a.rt=b.rt;
 	b.id=-1;
+	b.sig="b";
+	b.ins="c";
+	b.rd="d";
+	b.rs="e"; b.rt="f";
 }
+void assign2(stage &a, stage &b){  
+	a.id=b.id;
+	a.sig=signal(b.ins); // I think we need to flush rs,rd,rt of b because we are checking for the MEM stage
+	a.ins=b.ins;
+	a.rs=b.rs;
+	a.rd=b.rd;
+	a.rt=b.rt;
+}
+
 
  void MIPS_Architecture::executeCommandspipelined()
 {
+	
 	if (commands.size() >= MAX / 4)
 	{
 		handleExit(MEMORY_ERROR, 0);
 		return;
 	}
-
+    int counter=-1;
 	int clockCycles = 0;
-	stage IF;
-	stage ID;
-	stage EX;
-	stage MEM;
-	stage WB;
-	while (PCcurr < commands.size() )
+	int cycle=0;
+	stage IF,IF_prev; 
+	stage ID,ID_prev;
+	stage EX,EX_prev;
+	stage MEM,MEM_prev;
+	stage WB,WB_prev;
+	int n=commands.size();
+	while (counter < n )
 	{
+		cycle++;
 		++clockCycles;
-		vector<string> &command = commands[PCcurr];
-		if (instructions.find(command[0]) == instructions.end())
-		{
-			handleExit(SYNTAX_ERROR, clockCycles); // command not found
-			return;
-		}
+		cout<<"@@@@@@@ "<<counter<<endl;
+		// vector<string> command;
+		// if (counter<n-1 && IF.id==-1) { command= commands[counter+1];}
+		// else{
+		// 	 command = commands[counter];
+		// }
+		// for (auto x : command){
+		// 	cout<<x<<endl;
+		// }
+		// if (instructions.find(command[0]) == instructions.end())
+		// {
+		// 	handleExit(SYNTAX_ERROR, clockCycles); // command not found
+		// 	return;
+		// }
 		if (WB.id == -1 && MEM.id != -1)
 		{
 			WB.id = MEM.id;
+			assign2(MEM_prev,MEM);
 			assign( WB,  MEM);
+			WB.id=-1;
+			if (counter==n-1){counter++; cycle++;}
 		}
 		if (MEM.id == -1 && EX.id != -1)
 		{
+			assign2(EX_prev,EX);
             assign(MEM,EX);
+			
 		}
-		if (EX.id == -1 && ID.id != -1)
-		{  
-			if (MEM.id!=-1){
-			if ((detect_EX_hazard(EX,ID))==0 && detect_Mem_hazard(MEM,ID)==0) {
+		if (EX.id == -1 && ID.id != -1) 
+		{   cout<<((detect_EX_hazard(EX_prev,ID))==0 && detect_Mem_hazard(MEM,ID)==0)<<endl;
+			if ((detect_EX_hazard(EX_prev,ID))==0 && detect_Mem_hazard(MEM,ID)==0) {
+			assign2(ID_prev,ID);
             assign(EX,ID);
 			exit_code ret = (exit_code) instructions[EX.ins](*this, EX.rd, EX.rs,EX.rt);
 			if (ret != SUCCESS)
@@ -136,46 +168,42 @@ void assign(stage &a, stage &b){
 				handleExit(ret, clockCycles);
 				return;
 			}
-			++commandCount[PCcurr];
-		    PCcurr = PCnext;}}
-			else{
-				if ((detect_EX_hazard(EX,ID))==0) {
-            assign(EX,ID);
-			exit_code ret = (exit_code) instructions[EX.ins](*this, EX.rd, EX.rs,EX.rt);
-			if (ret != SUCCESS)
-			{
-				handleExit(ret, clockCycles);
-				return;
-			}
-			++commandCount[PCcurr];
-		    PCcurr = PCnext;}
+			++commandCount[counter];
+			if (EX.sig=="NA"){ counter=PCnext-1;}
+		    // counter = PCnext;
 			}
 		}
 		if (ID.id == -1 && IF.id != -1)
 		{
+			assign2(IF_prev,ID_prev);
            assign(ID,IF);
 		}
-		if (IF.id == -1)
-		{
-			vector<string> &command = commands[PCcurr];
-			IF.id=PCcurr;
+		if (IF.id == -1 && counter<n-1)
+		{   
+			// cout<<"HI"<<endl;
+			counter++;
+			vector<string> command = commands[counter];
+			
+			IF.id=counter;
 			IF.sig=signal(command[0]);
 			IF.ins=command[0];
             IF.rd=command[1];
 			IF.rs=command[2];
 			IF.rt=command[3];
+			// counter=PCnext;
 			// if (PCbranch == -1)
 			// {
-			// 	PCcurr++;
+			// 	counter++;
 			// }
 			// else
 			// {
-			// 	PCcurr = PCbranch;
+			// 	counter = PCbranch;
 			// 	PCbranch = -1;
 			// }
 		}
 		printRegisters(clockCycles);
 	}
+	cout<<"cycle: "<<cycle<<endl;
 	handleExit(SUCCESS, clockCycles);
 }
 
