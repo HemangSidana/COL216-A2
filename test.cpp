98c01d091e07a7686312803f4454a83f12bc45e6
satyam <satyamatal1000@gmail.com>
2023-04-09 15:21:22 +0530

    fdioji

--- test.cpp
+++ test.cpp
@@ -1,7 +1,9 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-
+// indicator to know which stage is occupied or free
+// when to remove the stall 
+//
 struct InstructionFetch(){
    string sig;
       string  ins;
@@ -81,23 +83,6 @@ int detect_lw_hazard(InstructionDecode a,InstructionFetch b){
 
 
 int main(){
-        map<string, tuple<string,string>> mymap;
-		mymap["IF"]=(make_tuple("","0"));
-		mymap["ID"]=(make_tuple("","0"));
-        mymap["EX"]=(make_tuple("","0"));
-        mymap["MEM"]=(make_tuple("","0"));
-        mymap["WB"]=(make_tuple("","0"));
-        std::map<std::string, tuple<string,string>>::iterator it = mymap.begin();
-        mymap["IF"] = make_tuple("","1");
-        if (get<1>mymap["IF"]=="1"){
-          cout<<123<<endl;
-        }
-  // Iterate through the map and print the elements
-  while (it != mymap.end())
-  {
-    cout << "Key: " << it->first << ", Value: "<< get<1>(it->second) << endl;
-    ++it;
-  }
 
     return 0;
 }
