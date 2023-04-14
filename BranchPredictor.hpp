4746d7118ea057e7f0c6610e31ab173499f750cb
satyam <satyamatal1000@gmail.com>
2023-04-14 17:01:46 +0530

    Oh god help us

--- BranchPredictor.hpp
+++ BranchPredictor.hpp
@@ -1,9 +1,78 @@
-#ifndef __BRANCH_PREDICTOR_HPP__
-#define __BRANCH_PREDICTOR_HPP__
-
+#ifndef _BRANCH_PREDICTOR_HPP_
+#define _BRANCH_PREDICTOR_HPP_
+#include <bits/stdc++.h>
 #include <vector>
 #include <bitset>
 #include <cassert>
+using namespace std;
+
+int getBinaryLength(int n)
+{
+    int length = 0;
+    while (n > 0) {
+        length += 1;
+        n /= 2;
+    }
+    return length;
+}
+int concat(int m, int n)
+{
+    // find binary length of n
+    int length = getBinaryLength(n);
+ 
+    // left binary shift m and then add n
+    return (m << length) + n;
+}
+
+bitset<2> ret(bitset<2> prev, bool taken){
+	if (taken)
+	{
+		if ( prev.to_ulong()==2)
+		{
+			prev.set(1);
+			prev.set(0);
+			return prev;
+		}
+		else if (prev.to_ulong()==3)
+		{
+			return prev;
+		}
+        else if (prev.to_ulong()==1)
+		{
+			prev.reset(0);
+			prev.set(1);
+			return prev;
+		}
+		else
+		{
+			prev.set(0);
+			prev.reset(1);
+			return prev;
+		}
+	}
+
+	else{
+		if (prev.to_ulong()==3)
+		{
+			prev.reset(0);
+			return prev;
+		}
+		else if (prev.to_ulong()==2)
+		{
+			prev.reset(1);
+			prev.set(0);
+			return prev;
+		}
+		else
+		{
+			prev.reset(0);
+			prev.reset(1);
+			return prev;
+		}
+		}
+		
+}
+
 
 struct BranchPredictor {
     virtual bool predict(uint32_t pc) = 0;
@@ -13,14 +82,19 @@ struct BranchPredictor {
 struct SaturatingBranchPredictor : public BranchPredictor {
     std::vector<std::bitset<2>> table;
     SaturatingBranchPredictor(int value) : table(1 << 14, value) {}
-
     bool predict(uint32_t pc) {
-        // your code here
-        return false;
+        uint32_t pc1=pc & 0x3fff;
+        bitset<2> prev=table[pc1];
+        if (prev.test(1)){ 
+            return true;}
+        else{ return false; }
     }
 
     void update(uint32_t pc, bool taken) {
-        // your code here
+        uint32_t pc1=pc & 0x3fff;
+        bitset<2> prev=table[pc1];
+        prev=ret(prev,taken);
+        table[pc1]=prev;
     }
 };
 
@@ -30,12 +104,20 @@ struct BHRBranchPredictor : public BranchPredictor {
     BHRBranchPredictor(int value) : bhrTable(1 << 2, value), bhr(value) {}
 
     bool predict(uint32_t pc) {
-        // your code here
-        return false;
+        uint32_t pc1=bhr.to_ulong();
+        bitset<2> prev=bhrTable[pc1];
+        if (prev.test(1)){ 
+            return true;}
+        else{ return false; }
     }
 
     void update(uint32_t pc, bool taken) {
-        // your code here
+        uint32_t pc1=bhr.to_ulong();
+        bitset<2> prev=bhrTable[pc1];
+        prev=ret(prev,taken);
+        bhrTable[pc1]=prev;
+        bhr <<= 1;
+        bhr[0] = taken;
     }
 };
 
@@ -49,13 +131,64 @@ struct SaturatingBHRBranchPredictor : public BranchPredictor {
     }
 
     bool predict(uint32_t pc) {
-        // your code here
-        return false;
+        uint32_t pc1= concat(pc,bhr.to_ulong()) && 0xFFFF ;
+        bitset<2> prev=combination[pc1];
+        if (prev.test(1)){ 
+            uint32_t pc2=pc & 0x3fff;
+            bitset<2> pred=table[pc2];
+            if (pred.test(1)){ 
+                return true;}
+            else{ return false; }
+        }
+        else{
+            uint32_t pc2=bhr.to_ulong();
+            bitset<2> pred=bhrTable[pc2];
+            if (pred.test(1)){ 
+                return true;}
+            else{ return false; }
+        }
+
     }
 
     void update(uint32_t pc, bool taken) {
-        // your code here
+        uint32_t pc1= concat(pc,bhr.to_ulong()) && 0xFFFF ;
+        bitset<2> prev=combination[pc1];
+        uint32_t pc2=bhr.to_ulong();
+        bitset<2> prev2=bhrTable[pc2];
+        uint32_t pc3=pc & 0x3fff;
+        bitset<2> prev3=table[pc3];
+        bitset<2> ph=prev2 ^ prev3;
+        int gh=ph.to_ulong();
+        int check1=1,check2=1,check3=1,check4=1;
+        if(prev2.test(1) ^ taken){
+            check1=0;
+        }
+        if(prev3.test(1) ^ taken){
+            check2=0;
+        }
+        if (prev2.test(0) ^ taken){check3=0;}
+        if (prev3.test(0) ^ taken){check4=0;}
+        prev3=ret(prev3,taken);
+        table[pc3]=prev3;
+        prev2=ret(prev2,taken);
+        bhrTable[pc2]=prev2;
+        bhr <<= 1;
+        bhr[0] = taken;
+        if((!check1 && check2) || (check1==check2 && !check3 && check4)){
+            for (int i=0; i<gh;i++){
+                prev=ret(prev,1);
+            }
+
+        }
+        if((check1 && !check2) || (check1==check2 && !check4 && check3)){
+            for (int i=0; i<gh;i++){
+                prev=ret(prev,0);
+            }
+        }
+        combination[pc1]=prev;
     }
 };
+
+
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,52 +1,15 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <bits/stdc++.h>
+#include "BranchPredictor.hpp"
 
 using namespace std;
 
-bitset<2> ret(bitset<2> prev, bool taken){
-	if (taken)
-	{
-		if ( not(prev.test(1)))
-		{
-			prev.set(1, true);
-			return prev;
-			cout<<"hello"<<prev.test(0)<<endl;
-		}
-		else if (prev.test(1))
-		{
-			prev.set(1,true);
-		}
-		else
-		{
-			prev.set(0, true);
-			prev.set(1, false);
-		}
-	}
-
-	else{
-			if (prev.test(1) )
-		{
-			prev.set(1, false);
-		}
-		else if (prev.test(0))
-		{
-			prev.set(0, false);
-			prev.set(1,true);
-		}
-		else
-		{
-			prev.set(0, false);
-			prev.set(1, false);
-		}
-		}
-		return prev;
+int you(string s)
+{
+	unsigned int x;
+	std::stringstream ss;
+	ss << std::hex << s;
+	ss >> x;
+	return x;
 }
 
 int main()
@@ -56,24 +19,55 @@ int main()
 	bitset<2> c("01");
 	bitset<2> d("00");
 	bool taken = true;
-	bitset<2> prev= ret(a,taken);
-	bitset<2> prev1= ret(b,taken);
-	bitset<2> prev2= ret(c,taken);
-	bitset<2> prev3= ret(d,taken);
-	bitset<2> prev4= ret(a,false);
-	bitset<2> prev5= ret(b,false);
-	bitset<2> prev6= ret(c,false);
-	bitset<2> prev7= ret(d,false);
-	//  cout << "test() at index 0: " << noboolalpha
-	//  << prev.test(1) << endl;
-	cout<<prev.test(0)<<" "<<prev.test(1)<<endl;
-	cout<<prev1.test(0)<<" "<<prev1.test(1)<<endl;
-	cout<<prev2.test(0)<<" "<<prev2.test(1)<<endl;
-	cout<<prev3.test(0)<<" "<<prev3.test(1)<<endl;
-	cout<<prev4.test(0)<<" "<<prev4.test(1)<<endl;
-	cout<<prev5.test(0)<<" "<<prev5.test(1)<<endl;
-	cout<<prev6.test(0)<<" "<<prev6.test(1)<<endl;
-	cout<<prev7.test(0)<<" "<<prev7.test(1)<<endl;
-
+	// cout<<a.test(1)<<endl;
+	// cout<<b.test(1)<<endl;
+	// cout<<c.test(1)<<endl;
+	// cout<<d.test(1)<<endl;
+	// bitset<2> prev= ret(a,taken);
+	// bitset<2> prev1= ret(b,taken);
+	// bitset<2> prev2= ret(c,taken);
+	// bitset<2> prev3= ret(d,taken);
+	// bitset<2> prev4= ret(a,false);
+	// bitset<2> prev5= ret(b,false);
+	// bitset<2> prev6= ret(c,false);
+	// bitset<2> prev7= ret(d,false);
+	// //  cout << "test() at index 0: " << noboolalpha
+	// //  << prev.test(1) << endl;
+	// cout<<prev1.to_string()<<" fdd "<<endl;
+	// cout<<prev2.to_string()<<" fdd "<<endl;
+	// cout<<prev3.to_string()<<" fdd "<<endl;
+	// cout<<prev4.to_string()<<" fdd "<<endl;
+	// cout<<prev5.to_string()<<" fdd "<<endl;
+	// cout<<prev6.to_string()<<" fdd "<<endl;
+	// cout<<prev7.to_string()<<" fdd "<<endl;
+	SaturatingBHRBranchPredictor Pred(3,65536);
+	// Pred(0);
+	std::ifstream file("branchtrace.txt");
+	string str;
+	int count = 0;
+	int accuracy = 0;
+	while (getline(file, str))
+	{
+		string c = "";
+		string d = "";
+		for (int i = 0; i < 8; i++)
+		{
+			c += str[i];
+		}
+		d += str[9];
+		int y = you(d);
+		int x = you(c);
+		bool b = Pred.predict(x);
+		// cout<<str<<endl;
+		if (b == y)
+		{
+			accuracy++;
+		}
+		count++;
+		Pred.update(x, y);
+		// cout<<count<<endl;
+	}
+	cout << count << " " << accuracy << endl;
 	return 0;
 }
+
